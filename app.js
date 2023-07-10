892a157fcde18f276e99d06740aee1711c0e2c52
Candice Ariel <candicewangsa@gmail.com>
2023-07-11 01:33:34 +0700

    create box to choose character

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const { createWorker } = Tesseract;
 
 const progress = document.getElementById('progress')
 const textarea = document.getElementById('textarea')
-const canvas = document.getElementById("canvas")
+const canvas = document.getElementById("cv1")
 
 document.querySelector('input[type="file"]').onchange = function() {
     let img = this.files[0]
@@ -14,8 +14,38 @@ document.querySelector('input[type="file"]').onchange = function() {
     }
 }
 
+let last_mousex = last_mousey = 0;
+let mousex = mousey = 0;
+let mousedown = false;
+let rect = {};
+const ctx = cv1.getContext("2d");
+
+cv1.addEventListener("mouseup", function (e) {
+  mousedown = false;
+}, false);
+
+cv1.addEventListener("mousedown", function (e) {
+  last_mousex = parseInt(e.clientX-cv1.offsetLeft);
+  last_mousey = parseInt(e.clientY-cv1.offsetTop);
+  mousedown = true;
+}, false);
+
+cv1.addEventListener("mousemove", function (e) {
+  mousex = parseInt(e.clientX-cv1.offsetLeft);
+  mousey = parseInt(e.clientY-cv1.offsetTop);
+  if(mousedown) {
+      ctx.beginPath();
+      var width = mousex-last_mousex;
+      var height = mousey-last_mousey;
+      ctx.rect(last_mousex,last_mousey,width,height);
+      rect = {x: last_mousex, y: last_mousey, width, height};
+      ctx.strokeStyle = 'red';
+      ctx.lineWidth = 2;
+      ctx.stroke();
+  }
+}, false);
+
 function drawImage(url) {
-  let ctx = canvas.getContext('2d')
   let image = new Image()
   image.src = url
   image.onload = () => {
@@ -35,20 +65,25 @@ function drawImage(url) {
   }
 }
 
-async function scanImg(url){
-  const worker = await createWorker({
-    logger: (m) => {
-      console.log(m);
-    },
-  });
+function scanImg(url){
+  (async () => {
+    const worker = await createWorker({
+      logger: (m) => {
+        console.log(m);
+      },
+    });
+    await worker.load();
+    await worker.loadLanguage('eng'); // 2
+    await worker.initialize('eng');
+    await worker.setParameters({preserve_interword_spaces: '1'})
+    const {
+      data: { text },
+    } = await worker.recognize(url);
+    textarea.innerHTML = text;
+    await worker.terminate();
+  })();
+}
 
-  await worker.loadLanguage('eng'); // 2
-  await worker.initialize('eng');
-
-  await worker.setParameters({preserve_interword_spaces: '1'})
-  const {
-    data: { text },
-  } = await worker.recognize(url);
+function insertTextToDom(text) {
   textarea.innerHTML = text;
-  await worker.terminate();
 }
